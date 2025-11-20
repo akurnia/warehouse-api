b93ed024d08aa0b090bdecb706109e71a917aa06
akurnia <akurnia.irawan@gmail.com>
2025-11-20 09:04:46 +0700

    Initial commit for warehouse-api

---
+++ src/test/java/com/arief/warehouse/warehouse_api/WarehouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arief.warehouse.warehouse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
